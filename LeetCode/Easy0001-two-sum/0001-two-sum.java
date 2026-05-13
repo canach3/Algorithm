c54314ac5b90fc8098b9b20403bd3788182cd30b
김정윤 <canach3@gmail.com>
2026-05-13 16:08:02 +0900

    Time: 45 ms (24.21%), Space: 47.3 MB (11.33%) - LeetHub

---
+++ LeetCode/Easy0001-two-sum/0001-two-sum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int a = nums[i];
+                int b = nums[j];
+
+                if (a + b == target) return new int[]{i, j};
+            }
+        }
+
+        return new int[]{0, 0};
+    }
+}
