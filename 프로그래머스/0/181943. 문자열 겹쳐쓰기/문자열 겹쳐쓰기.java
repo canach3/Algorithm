85a417de239985239c24138626202b2faadb2011
김정윤 <canach3@gmail.com>
2026-06-16 09:22:37 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.08 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < s; i++) {
+            sb.append(my_string.charAt(i));
+        }
+        
+        for (int i = 0; i < overwrite_string.length(); i++) {
+            sb.append(overwrite_string.charAt(i));
+        }
+        
+        for (int i = s + overwrite_string.length(); i < my_string.length(); i++) {
+            sb.append(my_string.charAt(i));
+        }
+        
+        return sb.toString();
+    }
+}
