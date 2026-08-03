b74c89bd5170a78e996fcc034958dea98c5c619e
김정윤 <canach3@gmail.com>
2026-08-03 16:06:23 +0900

    [level 1] Title: 유연근무제, Time: 0.33 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/1/388351. 유연근무제/유연근무제.java
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -1,8 +1,6 @@
 import java.util.*;
 
 class Solution {
-    static String[] days = {"월","화","수","목","금","토","주"};
-    
     public int solution(int[] schedules, int[][] timelogs, int startday) {
         int n = schedules.length;
         
@@ -23,19 +21,14 @@ class Solution {
             for (int j = 0; j < 7; j++) {
                 int day = (startday - 1 + j) % 7;
                 
-                // System.out.print(days[day]);
-                
                 // 주말 제외
                 if (day >= 5) continue; 
                 
                 if (timelogs[i][j] > schedules[i]) {
-                    // System.out.print("기준 : " + schedules[i] + ", 나 : " + timelogs[i][j]);
                     isOK = false;
                     break;
                 }
             }
-            
-            // System.out.println();
             
             if (isOK) cnt++;
         }
