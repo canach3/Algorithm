06e1c03d5310c83a389e2a8f7d344eab7006881a
김정윤 <canach3@gmail.com>
2026-08-06 15:01:37 +0900

    [level 1] Title: 명예의 전당 (1), Time: 2.22 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int [] result = new int[score.length];
+        
+        for (int i = 0; i < score.length; i++) {
+            pq.add(score[i]);
+            if (pq.size() > k) pq.poll();
+            
+            result[i] = pq.peek();
+        }
+        
+        return result;
+    }
+}
