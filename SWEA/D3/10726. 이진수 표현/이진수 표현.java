606f8a60a918305637dad6ccf9017732d889f88a
김정윤 <canach3@gmail.com>
2026-08-15 16:42:14 +0900

    [D3] Title: 이진수 표현, Time: 116 ms, Memory: 34,048 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.java
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.java
@@ -2,26 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-    static final int TARGET_MASK = (1 << 10) - 1;
-    public static void main(String[] args) throws IOException{
-//        System.setIn(new FileInputStream("res/input.txt"));
+	public static void main(String[] args) throws Exception{
+//		System.setIn(new FileInputStream("res/input.txt"));
+		
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringBuilder sb = new StringBuilder();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int testCase = 1; testCase <= T; testCase++) {
+			sb.append("#").append(testCase).append(" ");
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
 
-        int T = Integer.parseInt(br.readLine().trim());
-        for (int testCase = 1; testCase <= T; testCase++) {
-            sb.append("#").append(testCase).append(" ");
-
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
-
-            int target = (1 << N) - 1;
-
-            sb.append((M & target) == target ? "ON" : "OFF").append("\n");
-        }
-
-        System.out.print(sb);
-    }
+			int target = (1 << N) - 1;
+			
+			if ((M & target) == target) {
+				sb.append("ON");
+			} else {
+				sb.append("OFF");
+			}
+			
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+	}
 }
