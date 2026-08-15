dc4d1cf71c3b198889d6597cad4391816ef803d4
김정윤 <canach3@gmail.com>
2026-08-15 17:05:47 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 165 ms, Memory: 37,356 KB -BaekjoonHub

--- SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
@@ -2,63 +2,65 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-    static int x;
-    static int y;
+	public static void main(String[] args) throws Exception{
+//		System.setIn(new FileInputStream("res/input.txt"));
+		
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException{
-//        System.setIn(new FileInputStream("res/input.txt"));
+		for (int testCase = 1; testCase <= 10; testCase++) {
+			sb.append("#").append(testCase).append(" ");
 
-        StringBuilder sb = new StringBuilder();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			List<Integer> list = new ArrayList<>();
 
-        for (int testCase = 1; testCase <= 10; testCase++) {
-            sb.append("#").append(testCase).append(" ");
-
-            int N = Integer.parseInt(br.readLine());
-            List<Integer> list = new LinkedList<>();
-
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) {
-                list.add(Integer.parseInt(st.nextToken()));
-            }
-
-            int M = Integer.parseInt(br.readLine());
-
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < M; i++) {
-                char command = st.nextToken().charAt(0);
-
-                switch (command) {
-                    case 'I':
-                        x = Integer.parseInt(st.nextToken());
-                        y = Integer.parseInt(st.nextToken());
-                        for (int j = 0; j < y; j++) {
-                            list.add(j + x, Integer.parseInt(st.nextToken()));
-                        }
-                        break;
-                    case 'D':
-                        x = Integer.parseInt(st.nextToken());
-                        y = Integer.parseInt(st.nextToken());
-                        for (int j = 0; j < y; j++) {
-                            list.remove(x);
-                        }
-                        break;
-                    case 'A':
-                        y = Integer.parseInt(st.nextToken());
-                        for (int j = 0; j < y; j++) {
-                            list.add(Integer.parseInt(st.nextToken()));
-                        }
-                        break;
-                }
-            }
-
-            for (int i = 0; i < 10; i++) {
-                sb.append(list.get(i)).append(" ");
-            }
-
-            sb.append("\n");
-        }
-
-        System.out.print(sb);
-    }
+			int N = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				list.add(Integer.parseInt(st.nextToken()));
+			}
+			
+			int M = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < M; i++) {
+				char command = st.nextToken().charAt(0);
+				int x = 0;
+				int y = 0;
+				
+				switch(command) {
+					case 'I':
+						x = Integer.parseInt(st.nextToken());
+						y = Integer.parseInt(st.nextToken());
+						
+						for (int j = 0; j < y; j++) {
+							list.add(j + x, Integer.parseInt(st.nextToken()));
+						}
+						
+						break;
+					case 'D':
+						x = Integer.parseInt(st.nextToken());
+						y = Integer.parseInt(st.nextToken());
+						
+						for (int j = 0; j < y; j++) {
+							list.remove(x + y - j);
+						}
+						break;
+					case 'A':
+						y = Integer.parseInt(st.nextToken());
+						
+						for (int j = 0; j < y; j++) {
+							list.add(Integer.parseInt(st.nextToken()));
+						}
+						break;
+				}
+			}
+			
+			for (int i = 0; i < 10; i++) {
+				sb.append(list.get(i)).append(" ");
+			}
+			
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+	}
 }
