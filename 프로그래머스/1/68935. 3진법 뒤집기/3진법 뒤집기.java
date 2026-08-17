3d654690f48a806e3ea14f45c2774bab8183367d
김정윤 <canach3@gmail.com>
2026-08-18 08:36:50 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -3,9 +3,6 @@ import java.util.*;
 class Solution {
     public int solution(int n) {
         StringBuilder sb = new StringBuilder();
-        sb.append(Integer.toString(n, 3));
-        sb.reverse();
-        
-        return Integer.parseInt(sb.toString(), 3);
+        return Integer.parseInt(sb.append(Integer.toString(n, 3)).reverse().toString(), 3);
     }
 }
